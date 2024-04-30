05323fb21ab574befd209340927b9aaf5deb31dc
Candela Landi <landicandela01@gmail.com>
2024-04-30 08:42:32 -0600

    add login.dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs'
+
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -14,14 +18,21 @@ export class AuthService {
 
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-
+    console.log({ createUserDto })
     try {
-      const newUser = new this.userModel(createUserDto);
+      let { password, ...userData } = createUserDto;
 
-      // 1- Encriptar las contrasenas
+      let newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      })
+      console.log(newUser)
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
+
+      // 1- Encriptar la contrasena
       // 2 - Guardar el usuario
-
-      return newUser.save();
 
     } catch (error) {
       if (error.code === 11000) {
@@ -31,6 +42,38 @@ export class AuthService {
     }
 
   }
+
+
+  async login(loginDto: LoginDto) {
+
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email });
+    if (!user) {
+      throw new UnauthorizedException('Not valid credentials - email')
+    }
+
+    if (!bcryptjs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Not valid credentials - password ')
+    }
+
+    const { password: _, ...rest } = user.toJSON();
+
+    return {
+      user: rest,
+      token: 'ABC-123'
+    }
+
+    /**
+     * User { _id, name, email, roles }
+     * Token -> ASADSADA.ASDA.ASDAS
+     */
+  }
+
+
+
+
+
 
   findAll() {
     return `This action returns all auth`;

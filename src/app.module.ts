bfe555077e33f0fe62dd6b4312e89339e1b72d2c
Candela Landi <landicandela01@gmail.com>
2024-05-01 10:28:25 -0300

    fixing d

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-
     ConfigModule.forRoot(),
 
     MongooseModule.forRoot( process.env.MONGO_URI ),
 
-    AuthModule
+    AuthModule,
+
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+}

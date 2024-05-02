4affb7aaf1c79ee6b9ef47a593206019d8ef492f
Candela Landi <landicandela01@gmail.com>
2024-05-02 08:10:41 -0600

    finish backend

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, CreateUserDto, RegisterUserDto, UpdateAuthDto } from './dto/index';
+import { AuthGuard } from './guards/auth/auth.guard';
+import { User } from './entities/user.entity';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
@@ -21,23 +23,41 @@ export class AuthController {
     return this.authService.register(registerUserDto);
   }
 
+
+  @UseGuards(AuthGuard)
   @Get()
-  findAll() {
+  findAll(@Request() req: Request) {
+    const user = req['user'];
+
     return this.authService.findAll();
   }
+  //LoginResponse
+  @UseGuards(AuthGuard)
+  @Get('check-token')
+  checkToken( @Request() req: Request ) {
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+    const user = req['user'] as User;
+
+    return {
+      user, 
+      token: this.authService.getJwtToken({ id: user._id })
+    }
+
+
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
 }
+
+  /*   @Get(':id')
+    findOne(@Param('id') id: string) {
+      return this.authService.findOne(+id);
+    }
+  
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+      return this.authService.update(+id, updateAuthDto);
+    }
+  
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+      return this.authService.remove(+id); */

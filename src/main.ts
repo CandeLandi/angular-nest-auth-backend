e9732fd09c612c166137d674f97c4997ee82092a
Candela Landi <landicandela01@gmail.com>
2024-06-03 18:33:18 -0300

    add cors config

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://login-cl.netlify.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({

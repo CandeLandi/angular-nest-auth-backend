290863cbb43388851d167cc55d553a80326e628e
Candela Landi <landicandela01@gmail.com>
2024-06-08 18:58:35 -0300

    change database

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://login-cl.netlify.app', 
+    origin: 'https://login-cl.netlify.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   }
